39bd12515c750bff3f802f2800df386c7ab245f8
5C5 <colas.picard.etu@univ-lemans.fr>
2013-10-25 12:10:20 +0200

    ajout du second exercice du td sur les tubes

--- PSE/tdtube.c
+++ PSE/tdtube.c
@@ -15,12 +15,12 @@
 /*Énoncé : écrire en C : un fichier toto : Nom:Prenom:numero
  * recuperer le prenom a l'aide de cat <toto>| cut -d -F2 |sort */
 
-int td(int argc, char * argv[]){
+int exo1(int argc, char * argv[]){
 
     int tube1[2], tube2[2];
     char fichier[126];
 
-    strcpy(fichier, argv[1];);
+    strcpy(fichier, argv[1]);
     
     if(pipe(tube1) || pipe(tube2)){
 
@@ -48,7 +48,7 @@ int td(int argc, char * argv[]){
                      dup(tube2[1]);         //On associe l'entrée du tube à la première case libre de la table des descripteurs locaux, c'est à dire 1 qu'on vient de fermer
                      close(tube2[1]);       //On ferme l'accès en écriture au tube 2
 
-                     execlp("cut", "cut", -d: , "F2", NULL);    //on execute la commande cut en donnant la liste des paramètres.
+                     execlp("cut", "cut", "-d:" , "F2", NULL);    //on execute la commande cut en donnant la liste des paramètres.
                      printf("Problème sur l'éxécution de cut\n");
                   
                  }
@@ -89,3 +89,119 @@ int td(int argc, char * argv[]){
 
 }
 
+/*  Énoncé de l'exercice 2 un processus père lit des entrées au clavier. Il envoie les caractères à un processus fils 0 qui compte les occurences de chaque caractère, et envoie les chiffres à un processus fils 1 qui fait la somme des chiffres. Dans un deuxième temps, le fils 0 va renvoyer le tableau des occurences, et le fils 1 renverra la somme. Puis le père affichera le tableau et la somme */
+
+
+int exo2(){
+
+    int tube1[2], tube2[1];
+    char c;
+    int somme = 0, i = 0;              //somme des valeurs entrées au clavier
+    int occurence[26];          //tableau contenant les occurences des caractères lu au clavier
+
+    for(; i < 26; i ++){
+
+        occurence[i] = 0;
+    }
+    
+    if(pipe(tube1) || pipe(tube2)){
+
+        perror("Pb dans la création des tubes\n");
+        exit(-1);
+    }
+
+    switch(fork()){         //création du fils 0 (qui s'occupe des caractères)
+
+        case -1 :
+            printf("Pb sur le fork du fils 0");
+            exit(-1);
+            break;
+
+        case 0 :
+
+            close(tube2[0]);
+            close(tube2[1]);    //On ferme le tube 2 car on en a pas besoin ici
+
+            //phase 1
+            do {
+
+                read(tube1[0], &c, 1)
+                occurence[c - 'a'] += 1;
+            } while(c != 'X');
+
+            //Phase 2
+            write(tube1[1], occurence, sizeof(int)* 26);        //on met le tableau avec les valeurs dans le tube 1
+
+            close(tube1[0]);
+            close(tube1[1]);
+
+            break;
+
+        default :
+
+            switch(fork()){
+                
+                case -1 :
+                    printf("Pb sur le fork du fils 1\n");
+                    exit(-1);
+                    break;
+
+                case 0 :
+
+                    close(tube1[0]);
+                    close(tube1[1]);        //n'utilise pas le tube 1
+
+                    //phase 1
+                    do {
+
+                        read(tube2[0], &c, 1)
+                        somme += c - '0' ;
+                    } while (c != 'X');
+
+                    close(tube2[0]);
+
+                    // Phase 2
+                    write(tube2[1], &somme, sizeof(int));
+                    close(tube2[1]);
+                    break;
+
+                default :
+
+                    //phase 1
+                    while((c = getchar()) != EOF){
+
+                        if((c =< 'z') && (c >='a')){
+                            
+                            write(tube1[1], &c, 1);
+                        }
+                        else{
+                            
+                            if((c <= '9') && (c >= '0')){
+
+                                write(tube2[1], &c, 1);
+                            }
+                        }
+                    }
+
+                    write(tube1[1], 'X', 1);
+                    close(tube1[1]);
+                    write(tube2[1], 'X', 1);
+                    close(tube2[1]);
+
+                    //Phase 2
+                    read(tube2[0], occurence, sizeof(int)*26);
+                    read(tube1[0], somme, sizeof(int));
+                    close(tube2[0]);
+                    close(tube1[0]);
+
+                    //Affichage des valeurs
+                    for(i = 0; i< 26, i++){
+
+                        printf("%c = %d\n", 'a'+i, occurence[i]);
+                    }
+                    printf("somme = %d\n", somme);
+            }
+
+    }
+    return EXIT_SUCCESS;
+}
