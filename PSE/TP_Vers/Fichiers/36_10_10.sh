c421ec18a3ff86bd1274572f793db8a28c36058a
r00ter <antoine.fouc@gmail.com>
2014-01-14 15:59:44 +0100

    Supression des dossiers dorénavant inutiles

--- PSE/TP_Vers/Fichiers/36_10_10.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-../Terrain/init_fich_terrain terrain01 10 10
-#../Verrous/vision ../Fichiers/terrain01 &
-../Verrous/ver ../Fichiers/terrain01 M & ../Verrous/ver ../Fichiers/terrain01 X & ../Verrous/ver ../Fichiers/terrain01 T & ../Verrous/ver ../Fichiers/terrain01 H & ../Verrous/ver ../Fichiers/terrain01 Z
--- PSE/TP_Vers/Fichiers/lancement_vision.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../Verrous/vision ../Fichiers/terrainO1
