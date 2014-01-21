28314a3e13625eea482fe3fd9cb702baf635fe8d
r00ter <antoine.fouc@gmail.com>
2014-01-21 20:27:38 +0100

    Ajout de tests en rapport avec le 2ème cours de Java

--- Semestre 2/Java/Entrainement/Autre/1/Test.java
+++ Semestre 2/Java/Entrainement/Autre/1/Test.java
@@ -4,12 +4,14 @@ public class Test{
 	
 		System.out.println("Début du programme\n");
 		
+		/*Test du polymorphisme ad-hoc (type des paramètres)*/
 		methode("hello");
 		methode(1);
 		methode(true);
 		
 		System.out.println("\n");
 		
+		//Test sur l'héritage
 		Prems a = new Prems();
 		Deuxs b = new Deuxs();
 		
@@ -17,9 +19,36 @@ public class Test{
 		b.maMeth();
 		b.maMethperso();
 		
+		System.out.println("\n");
+		
+		//Test sur le polymorphisme d'héritage & .co
+		Autre c = new Autre();
+		Autre d = new Autre("Une autre phrase");
+		Autre e = new Autre("Première phrase", "Deuxième phrase");
+		
+		c.afficheToi();
+		d.afficheToi();
+		e.afficheToi();
+		
+		System.out.println("\n");
+		
+		//Test sur la redéfinition
+		a.afficheAttributs();
+		b.afficheAttributs();
+		
+		System.out.println("\n");
+		
+		//Test upcasting		
+		a.toString();
+		b.toString();
+		
+		Prems f = b; //Upcasting
+		f.toString();	
+		
 		System.exit(0);	
 	}
 	
+	/*Méthodes crées pour voir le polymorphisme de type*/
 	private static void methode(String s){
 	
 		System.out.println("Une chaîne");
@@ -36,23 +65,99 @@ public class Test{
 	}
 }
 
+/*Test de l'héritage et du polymorphisme d'héritage*/
 class Prems{
 
-	public static void maMeth(){
+	/*Déclaration des var. d'intance en "protected" pour autoriser l'accès aux classes filles*/
+	protected int x;
+	protected int y;
+	
+	public void maMeth(){
 	
 		System.out.println("Une classe prems");
 	}
 	
-	public static void maMethperso(){
+	public void maMethperso(){
 	
 		System.out.println("Méthode d'une prem's");
+	}
+	
+	/*Ajouter "final" pour être sur que cette méthode est appelée lors de la liaison dynamique (on part de la classe mère et on descend le plus bas possible dans l'arborescence tant qu'on rencontre une redéfinition de cette méthode sauf si on rencontre "final" -> execution de cette méthode)*/
+	public void afficheAttributs(){
+	
+		System.out.println(this.x + " et " + this.y);
+	}
+	
+	public Prems(){
+	
+		this.x = 33;
+		this.y = 66;
+	}
+	
+	public void toString(){
+	
+		System.out.println("["+this.x+", "+this.y+"]");
 	}
 }
 
 class Deuxs extends Prems{
-
-	public static void maMeth(){
+	
+	/*Pas de classe fille, on met en "private*/
+	private int z;
+	
+	public Deuxs(){
+	
+		super();
+		this.x = 22;
+		this.z = 99;
+	}
+	
+	public void maMeth(){
 	
 		System.out.println("Une classe deuxs");
+	}
+	
+	public void afficheAttributs(){
+	
+		super.afficheAttributs();
+		System.out.println(" et " + this.z);
+	}
+	
+	public void toString(){
+		super.toString();
+		System.out.println("["+this.z+"]");
+	}
+}
+
+/*Test des constructeurs*/
+class Autre{
+
+	private String s;
+	private String s2;
+	
+	public Autre(String phrase, String phrase2){
+	
+		this(phrase);
+		this.s2 = new String(phrase2);
+	}
+	
+	public Autre(String phrase){
+	
+		this.s = new String(phrase);
+	}
+	
+	public Autre(){
+	
+		this.s = new String("Phrase par défaut");
+	}
+	
+	public void afficheToi(){
+	
+		System.out.println(this.s);
+		
+		if(s2 != null){
+		
+			System.out.println(this.s2);
+		}
 	}
 }
