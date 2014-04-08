5c5b571398b965ffbbea28adff16653185908a34
colas <colas.picard@laposte.net>
2014-04-08 12:15:31 +0200

    ajout de l exo sur les GUI en JAVA

---
+++ Semestre 2/Java/TD/TDGUI/td.java
@@ -0,0 +1,43 @@
+import java.awt.*
+import javax.swing.*
+public class Exo extends JFrame{
+
+    public static void apparitionFenetre(){new Exo();}
+
+    public Exo(){
+
+        this.setTitle("Creation - suppression de boutons");
+
+        JPanel monpanel = new JPanel();
+        monpanel.addActionListener(new MonActionListener());
+        monpanel.setLayout(new BorderLayout());
+        JButton boutonCrea = new JButton("Creation");
+        JButton boutonSup = new JButton("Suppression");
+        monpanel.add(boutonCrea, BorderLayout, NORTH);
+        monpanel.add(boutonSup, BorderLayout, SOUTH);
+        boutonCrea.addActionListener(new MonActionListener());
+        boutonSup.addActionListener(new MonActionListener());
+        this.getContentPanel().add(monpanel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public class MonActionListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent a){
+
+            if(getSource() = boutonCrea){
+            }
+            else {
+        }
+    }
+
+    public static main(String args[]){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                apparitionFenetre();
+            }
+        });
+    }
+}
