4b0f34d8fc0b3dfbd599766f3c067046d125d10c
5C5 <colas.p.s@gmail.com>
2014-04-09 12:04:17 +0200

    Ajout du asynt.h pour le tp2 suisse

---
+++ Semestre 2/LangageCompilation/TP/TP1 - 2/Asynt.h
@@ -0,0 +1,15 @@
+#ifndef ASYNT_H
+#define ASYNT_H
+
+int bFF(int, int *);
+
+int bIdent(int, int *);
+
+int bReel(int, int *);
+
+int bCode(int, int *);
+
+int bLigne(int, int *);
+
+int bProg(int, int *);
+#endif
